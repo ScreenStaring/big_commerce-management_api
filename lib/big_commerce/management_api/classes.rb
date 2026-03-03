69b44a4d61dd0dcdc88f6115c4616254e3ce9d61
sshaw <skye.shaw@gmail.com>
2026-03-03 15:47:10 -0500

    Convert Product price attributes to BigDecimal

--- lib/big_commerce/management_api/classes.rb
+++ lib/big_commerce/management_api/classes.rb
@@ -2,6 +2,7 @@
 
 require "class2"
 require "json"
+require "bigdecimal"
 
 # This results in certain properties in the JSON samples/definitions to be parsed as
 # Time which class2 will see and convert the attribute values to Time
@@ -23,6 +24,22 @@ class2 "BigCommerce::ManagementAPI", JSON.parse(classes, :create_additions => tr
 
   def headers=(headers)
     @headers = headers
+  end
+end
+
+class BigCommerce::ManagementAPI::Product
+  DECIMAL_PROPERTIES = %i[price cost_price retail_price sale_price map_price calculated_price fixed_cost_shipping_price].freeze
+
+  # class2 giveth and class2 takith away
+  alias_method :initialize_without_decimals, :initialize
+
+  def initialize(*args)
+    initialize_without_decimals(*args)
+
+    DECIMAL_PROPERTIES.each do |prop|
+      value = instance_variable_get("@#{prop}")
+      instance_variable_set("@#{prop}", BigDecimal(value.to_s)) if value
+    end
   end
 end
 
